dcd3c4a7069c5a634b4f9926b3d75156d6a10c32
Julián <juliancastrillongiraldo@gmail.com>
2022-03-04 18:59:40 -0500

    Definir componentes de navegacion

---
+++ src/components/Molecules/NavItems.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const NavItem = () => (
+    <div>
+        <Link to='/about' > Acerca de </Link>
+    </div>
+);
+
+
+export default NavItem;
--- src/components/organisms/Navigation.jsx
+++ src/components/organisms/Navigation.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBrand from "../Molecules/NavBrand";
+import NavItem from "../Molecules/NavItems";
 
 export const Navigation = () => (
 
     <nav>
         <NavBrand />
+        <NavItem />
     </nav>
 );
 export default Navigation;    
