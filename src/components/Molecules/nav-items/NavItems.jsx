533f56e5d1e952c5be04f9cb91a1337154ca326d
Julián <juliancastrillongiraldo@gmail.com>
2022-03-14 18:41:38 -0500

    Decouple nav items management

--- src/components/Molecules/nav-items/NavItems.jsx
+++ src/components/Molecules/nav-items/NavItems.jsx
@@ -1,12 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './NavItems.css';
+import "./NavItems.css";
+import { PropTypes } from "react-PropTypes";
 
-const NavItem = () => (
-    <div className="m-nav-items" >
-        <Link to='/about' className="m-nav-items__item"  > About </Link>
-    </div>
-);
+const NavItems = (props) => {
+
+    /**
+     * Build navigation items for navigation
+     * @param {{text: string, target: string}} value  
+     * @param {number} index 
+     * @returns {JSX.Element}
+     */
+
+  const buildItem = (value, index) => (
+      <Link className="m-nav-items__item" to={value.target} key={index}> {value.text}</Link>
+    );
 
 
+      /**
+     * Build navigation items for navbarv
+     * @param {{text: string, target: string}{}}      items  
+     * @returns {JSX.Element}
+     */
+
+    const buildItems = () => (props.items ? props.items.map(buildItem): <></>);
+    return <div className="m-nav-items">{buildItems()}</div>;
+};
+
+NavItem.prototypes = {
+    items: PropTypes.shape({
+    text: PropTypes.string,
+    target: PropTypes.string,
+}),
+
+};
 export default NavItem;
--- src/components/organisms/Navigation/Navigation.jsx
+++ src/components/organisms/Navigation/Navigation.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import NavBrand from "../../Molecules/nav-brand/NavBrand";
 import NavItem from "../../Molecules/nav-items/NavItems";
 import "./Navigation.css";
+import {navItems}from "../../../assets/js/navItems";
 
 
 export const Navigation = () => (
 
     <nav className="o-navigation" >
         <NavBrand />
-        <NavItem />
+        <navItems items={navItems} />
     </nav>
 );
 export default Navigation;    
