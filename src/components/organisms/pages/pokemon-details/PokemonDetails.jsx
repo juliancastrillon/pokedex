1715ae91e5fe1e3a62e95326db438dc06d62838d
Julián <juliancastrillongiraldo@gmail.com>
2022-03-18 08:30:34 -0500

    type definition at NavItems component

--- src/components/organisms/pages/pokemon-details/PokemonDetails.jsx
+++ src/components/organisms/pages/pokemon-details/PokemonDetails.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import DefaultLayout from "../../../layouts/default-layout/DefaultLayout";
 
-const PokemonDetails = () => (
-<DefaultLayout title='Drtalle'>
 
-    <h1>Pokemon details page</h1>
+const PokemonDetails = () => {
 
-</DefaultLayout>
+return(
+    <DefaultLayout title='Drtalle'>
+        <h1>Pokemon details page</h1>
+    </DefaultLayout>
     
-
-);
+)};
 
 export default PokemonDetails;
